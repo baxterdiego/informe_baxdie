95e054044ee34145b6ba021d090f64f8b7538a67
baxterdiego <baxterdiego22@gmail.com>
2026-05-25 19:29:47 -0400

    Modificado Responsabilidades.jsx

--- src/components/Responsabilidades.jsx
+++ src/components/Responsabilidades.jsx
@@ -1,13 +1,22 @@
+import ReactMarkdown from "react-markdown"
+import responsabilidades from "../docs/05_responsabilidades_baxdie.md?raw"
+
 function Responsabilidades() {
   return (
-    <section>
-      <h2 className="text-2xl font-bold mb-4">
+    <section className="mb-10">
+
+      <h2 className="text-3xl font-bold mb-4">
         Responsabilidades
       </h2>
 
-      <p>
-        Responsabilidades penales, civiles y administrativas.
-      </p>
+      <div className="border rounded p-6">
+
+        <ReactMarkdown>
+          {responsabilidades}
+        </ReactMarkdown>
+
+      </div>
+
     </section>
   )
 }
