6e77e35fa002823d164840574c14fa48dab5b6b6
baxterdiego <baxterdiego22@gmail.com>
2026-05-25 19:16:36 -0400

    Modificado Resumen.jsx

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,14 +1,22 @@
+import ReactMarkdown from "react-markdown"
+import resumen from "../docs/01_resumen_baxdie.md?raw"
+
 function Resumen() {
   return (
-    <section>
-      <h2 className="text-2xl font-bold mb-4">
+    <section className="mb-10">
+
+      <h2 className="text-3xl font-bold mb-4">
         Resumen Ejecutivo
       </h2>
 
-      <p>
-        Caso Equifax: brecha de seguridad ocurrida en 2017
-        con filtración masiva de información financiera.
-      </p>
+      <div className="border rounded p-6">
+
+        <ReactMarkdown>
+          {resumen}
+        </ReactMarkdown>
+
+      </div>
+
     </section>
   )
 }
