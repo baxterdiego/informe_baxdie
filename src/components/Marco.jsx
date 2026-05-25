6bc6f7e6b54569c8892d7503dc12892f128aa6f5
baxterdiego <baxterdiego22@gmail.com>
2026-05-25 18:53:18 -0400

    Componente Marco actualizado

--- src/components/Marco.jsx
+++ src/components/Marco.jsx
@@ -1,8 +1,14 @@
 function Marco() {
   return (
-    <div>
-      <h1>Marco</h1>
-    </div>
+    <section>
+      <h2 className="text-2xl font-bold mb-4">
+        Marco Normativo
+      </h2>
+
+      <p>
+        Ley 21.459, Ley 19.628, GDPR y regulaciones financieras.
+      </p>
+    </section>
   )
 }
 
