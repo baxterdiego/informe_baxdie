996d6a48139b06ad40a298203e4672600a41817d
baxterdiego <baxterdiego22@gmail.com>
2026-05-25 19:01:20 -0400

    App.jsx modificado

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import Resumen from './components/Resumen'
-import Marco from './components/Marco'
-import Delitos from './components/Delitos'
-import Comparacion from './components/Comparacion'
-import Responsabilidades from './components/Responsabilidades'
-import Datos from './components/Datos'
-import Conclusiones from './components/Conclusiones'
-import Prompts from './components/Prompts'
+import Resumen from "./components/Resumen"
+import Marco from "./components/Marco"
+import Delitos from "./components/Delitos"
+import Comparacion from "./components/Comparacion"
+import Responsabilidades from "./components/Responsabilidades"
+import Datos from "./components/Datos"
+import Conclusiones from "./components/Conclusiones"
+import Prompts from "./components/Prompts"
 
 function App() {
   return (
-    <div>
+    <div className="p-8 space-y-12">
+
+      <h1 className="text-4xl font-bold">
+        Caso Equifax – Análisis Legal y Ciberseguridad
+      </h1>
+
       <Resumen />
+
       <Marco />
+
       <Delitos />
+
       <Comparacion />
+
       <Responsabilidades />
+
       <Datos />
+
       <Conclusiones />
+
       <Prompts />
+
     </div>
   )
 }
