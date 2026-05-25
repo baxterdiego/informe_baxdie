a53baf32e94261feb8b14db090e4bd389eac4e76
baxterdiego <baxterdiego22@gmail.com>
2026-05-25 18:56:58 -0400

    Componente Datos actualizado

--- src/components/Datos.jsx
+++ src/components/Datos.jsx
@@ -1,8 +1,14 @@
 function Datos() {
   return (
-    <div>
-      <h1>Datos</h1>
-    </div>
+    <section>
+      <h2 className="text-2xl font-bold mb-4">
+        Datos Personales
+      </h2>
+
+      <p>
+        Aplicación de Ley 19.628 y derechos ARCO.
+      </p>
+    </section>
   )
 }
 
