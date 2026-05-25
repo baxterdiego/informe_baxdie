9e1b2e6c52fbc910bb7ee7f37ad06dc00dc5bdbb
baxterdiego <baxterdiego22@gmail.com>
2026-05-25 19:18:24 -0400

    Modificado Marco.jsx

--- src/components/Marco.jsx
+++ src/components/Marco.jsx
@@ -1,13 +1,22 @@
+import ReactMarkdown from "react-markdown"
+import marco from "../docs/02_marco_baxdie.md?raw"
+
 function Marco() {
   return (
-    <section>
-      <h2 className="text-2xl font-bold mb-4">
+    <section className="mb-10">
+
+      <h2 className="text-3xl font-bold mb-4">
         Marco Normativo
       </h2>
 
-      <p>
-        Ley 21.459, Ley 19.628, GDPR y regulaciones financieras.
-      </p>
+      <div className="border rounded p-6">
+
+        <ReactMarkdown>
+          {marco}
+        </ReactMarkdown>
+
+      </div>
+
     </section>
   )
 }
